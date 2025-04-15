e6f67aaee2d492207e4b2b10c2bbe1d35e11e971
Louis-P35 <louis.polart@hotmail.fr>
2025-04-15 19:28:34 +0200

    Refacto

--- stm32/Quadcopter/Core/Src/quadcopter.cpp
+++ stm32/Quadcopter/Core/Src/quadcopter.cpp
@@ -38,9 +38,6 @@ void DroneController::mainSetup()
 	// Setup the serial print
 	LogManager::getInstance().setup(m_huart_ext);
 
-
-	//m_imu.init(AccScale::_8G, GyroScale::DPS500);
-
 	// Setup the IMU (ICM20948)
 	icm20948_init(); // Accelerometer & gyroscope
 	ak09916_init();  // Magnetometer
@@ -57,7 +54,7 @@ void DroneController::mainSetup()
 	float ay = 0.0f;
 	float az = 1.0f;
 	// read accelerometer
-	m_EKF.initWithAcc(ax, ay, az);
+	m_EKF.initWithAcc(ax, ay, az); // Norm must not be 0
 }
 
 
@@ -106,66 +103,4 @@ void DroneController::mainLoop(const float dt)
 		"%7.2f, %7.2f, %7.2f, %2.7f\r\n",
 		roll, pitch, yaw, dt);
 	LogManager::getInstance().serialPrint(pBuffer);
-
-
-
-
-	/*Eigen::Vector3d gyroTmp = Eigen::Vector3d(0.1, 0.5, -0.1);
-	Eigen::Vector3d magnetoTmp = Eigen::Vector3d(0.0, 1.0, 0.0);
-	//Eigen::Vector3d accTmp = Eigen::Vector3d(0.1145, -0.9839, 0.1369);
-	//Eigen::Vector3i accTmpR = Eigen::Vector3i(m_imu.m_rawAcc.m_vect[0], m_imu.m_rawAcc.m_vect[1], m_imu.m_rawAcc.m_vect[2]);
-	Eigen::Quaterniond attitude = m_ahrs.computeAHRS(
-			m_imu.m_filteredAcceloremeter,
-			m_imu.m_filteredGyro,
-			//gyroTmp,
-			magnetoTmp,
-			dt
-			);
-
-	Eigen::Vector3d gyroOffsetDebug(0.0, 0.0, 0.0);
-
-	//Vector<double, 3> test(attitude2.m_q.m_vect[1], attitude2.m_q.m_vect[2], attitude2.m_q.m_vect[3]);
-	Eigen::Quaterniond attitude3 = m_ahrs3.computeAHRS(
-				m_imu.m_filteredAcceloremeter,
-				m_imu.m_filteredGyro,// + gyroOffsetDebug,
-				//gyroTmp,
-				magnetoTmp,
-				dt
-				);*/
-
-	//uint32_t sysClockFreq = HAL_RCC_GetSysClockFreq();
-
-	//Vector<int, 3> gyroTmp2((int)(m_imu.m_filteredGyro.m_vect[2]*1000.0), (int)(m_imu.m_rawScaledGyro.m_vect[2]*1000.0), (int)(m_imu.m_gyroOffset.m_vect[2]*1000.0));
-
-	/*double roll, pitch, yaw;
-	roll = pitch = yaw = 0.0;
-	Eigen::Vector3d vTmp0 = Utils::quaternionToEulerDeg(attitude);
-	Vector<int, 3> tmppp((int)vTmp0(0), (int)vTmp0(1), (int)vTmp0(2));
-
-	//attitude2.toEuler(roll, pitch, yaw);
-	Eigen::Vector3d vTmp = Utils::quaternionToEulerDeg(attitude2);
-	Vector<int, 3> tmppp2((int)vTmp(0), (int)vTmp(1), (int)vTmp(2));
-
-	//attitude3.toEuler(roll, pitch, yaw);
-	Vector<int, 3> tmppp3((int)roll, (int)pitch, (int)yaw);*/
-
-	//Vector<double, 3> tm(attitude2.m_q.m_vect[1], attitude2.m_q.m_vect[2], attitude2.m_q.m_vect[3]);
-	//LogManager::getInstance().serialPrint(attitude3);
-	//Vector<double, 3> tm2(attitude.m_q.m_vect[1], attitude.m_q.m_vect[2], attitude.m_q.m_vect[3]);
-	//LogManager::getInstance().serialPrint(m_imu.m_filteredAcceloremeter);
-	//LogManager::getInstance().serialPrint(tmppp, tmppp2, tmppp3);
-
-
-	//LogManager::getInstance().serialPrint(static_cast<int>(sysClockFreq / 1000000));
-
-	//char pBuffer[16] = "Hello world!";
-	//HAL_UART_Transmit(&m_huart_ext, (uint8_t*)pBuffer, 12, 100);
-
-	//uint8_t buffer[4];  // Buffer to hold bytes of the float
-
-	// Copy the float into the buffer
-	//memcpy(buffer, &m_imu.m_filteredAcceloremeter.m_vect[0], sizeof(float));
-
-	// Transmit the buffer over UART
-	//HAL_UART_Transmit(&m_huart_ext, buffer, sizeof(buffer), 100);
 }
